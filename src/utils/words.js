4be1054282a6608c91ed22b86953f3b996417b40
marcosdellavecchia <marcosdvecchia@gmail.com>
2021-05-02 19:19:45 -0300

    Transform words to lowercase

--- src/utils/words.js
+++ src/utils/words.js
@@ -3,6 +3,6 @@ import faker from "faker";
 export const generate = (count = 10) => {
   return new Array(count)
     .fill()
-    .map((_) => faker.random.word())
+    .map((_) => faker.random.word().toLowerCase())
     .join(" ");
 };
